f56eff39916c0db5bf266a74e1e3b13d5ab03ec0
Tony Boyles <Anthony@Boyles.cc>
2018-08-02 16:53:04 -0400

    Added interactive Table

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(readxl)
 library(shiny)
 library(lubridate)
 library(scales)
+library(rhandsontable)
 
 # Custom theme for making a clean Gantt chart
 theme_gantt <- function(base_size=11, base_family="Source Sans Pro Light") {
@@ -11,42 +12,84 @@ theme_gantt <- function(base_size=11, base_family="Source Sans Pro Light") {
           axis.title.x=element_text(vjust=-0.2), axis.title.y=element_text(vjust=1.5),
           title=element_text(vjust=1.2, family="Source Sans Pro Semibold"),
           panel.border = element_blank(), axis.line=element_blank(),
-          panel.grid.minor=element_blank(),
+          panel.grid.minor = element_blank(),
           panel.grid.major.y = element_blank(),
           panel.grid.major.x = element_line(size=0.5, colour="grey80"),
-          axis.ticks=element_blank(),
-          legend.position="bottom", 
-          axis.title=element_text(size=rel(0.8), family="Source Sans Pro Semibold"),
-          strip.text=element_text(size=rel(1), family="Source Sans Pro Semibold"),
-          strip.background=element_rect(fill="#ffffff", colour=NA),
-          panel.spacing.y=unit(1.5, "lines"),
+          axis.ticks = element_blank(),
+          legend.position = "bottom", 
+          axis.title = element_text(size=rel(0.8), family="Source Sans Pro Semibold"),
+          strip.text = element_text(size=rel(1), family="Source Sans Pro Semibold"),
+          strip.background = element_rect(fill="#ffffff", colour=NA),
+          panel.spacing.y = unit(1.5, "lines"),
           legend.key = element_blank())
 }
 
-ui <- fluidPage(
-  verticalLayout(
-    titlePanel("Gantt Chart-er"),
-    fileInput("file1", "Choose CSV File", multiple = FALSE,
-      accept = c(
-        "text/csv","text/comma-separated-values,text/plain",".csv",
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",".xlsx",
-        "application/vnd.ms-excel",".xls"
+ui <- navbarPage("Shiny Gantt Chart",
+  tabPanel("App",
+    column(3,
+      wellPanel(
+        fileInput("file1", "Choose Project Schedule Spreadsheet (CSV/XLSX)", multiple = FALSE,
+          accept = c(
+            "text/csv","text/comma-separated-values","text/plain",".csv",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",".xlsx",
+            "application/vnd.ms-excel",".xls"
+          )
+        ) #,
+        # selectInput('start', 'Start', c()),
+        # selectInput('end', 'End', c()),
+        # selectInput('project', 'Project', c()),
+        # selectInput('task', 'Task', c())
       )
     ),
-    plotOutput("gantt")
+    column(9,
+      tabsetPanel(
+        tabPanel("Table",
+          downloadButton('downloadData', 'Download'),
+          rHandsontableOutput("hot")
+        ),
+        tabPanel("Gantt",
+          downloadButton('downloadPlot', 'Download'),
+          plotOutput("gantt")
+        )
+      )
+    )
+  ),
+  tabPanel("About",
+    column(6, offset = 3,
+      wellPanel(includeMarkdown('README.md'))
+    )
   )
 )
 
-server <- function(input, output){
-  output$gantt <- renderPlot({
-    req(input$file1)
-    
-    if(input$file1$type %in% c("text/csv","text/comma-separated-values,text/plain",".csv")){
+server <- function(input, output, session){
+  output$downloadData <- downloadHandler(
+    filename = 'ShinyGanttExport.csv',
+    content = function(file){
+      input$hot %>%
+        hot_to_r() %>%
+        write_csv(file)
+    }
+  )
+  
+  output$hot <- renderRHandsontable({
+    if(is.null(input$file1)){
+      tasks <- tibble(Start = today(), End = today() + 30, Project = "Management", Task = "Gantt Charting")
+    } else if(input$file1$type %in% c("text/csv","text/comma-separated-values,text/plain",".csv")){
       tasks <- read_csv(input$file1$datapath)
     } else {
       tasks <- read_excel(input$file1$datapath)
     }
-
+    
+    tasks %>%
+      rhandsontable() %>%
+      hot_table(highlightCol = TRUE, highlightRow = TRUE)
+  })
+  
+  output$gantt <- renderPlot({
+    req(input$hot)
+    
+    tasks <- hot_to_r(input$hot)
+    
     tasks.long <- tasks %>%
       mutate(Start = ymd(Start), End = ymd(End)) %>%
       gather(date.type, task.date, -c(Project, Task)) %>%
@@ -64,6 +107,13 @@ server <- function(input, output){
       scale_y_date(date_breaks="2 months", labels=date_format("%b ‘%y")) +
       theme_gantt() + theme(axis.text.x=element_text(angle=45, hjust=1))
   })
+  
+  output$downloadPlot <- downloadHandler(
+    filename = "ShinyGanttExport.png",
+    content = function(file){
+      ggsave(file, width = 7.5, height = 5, units = 'in')
+    }
+  )
 }
 
 shinyApp(ui = ui, server = server)
